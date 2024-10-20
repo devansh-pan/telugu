ae4850ba696b0f945030be28dc02eb5d6fbf4f44
reader <143727212+devansh-pan@users.noreply.github.com>
2024-10-20 09:26:57 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Metadata, Site, Socials } from "@types";
 export const SITE: Site = {
   TITLE: "Telugu Lokam",
   DESCRIPTION: "Welcome to telugu lokam site.",
-  EMAIL: "trevortylerlee@gmail.com",
+  EMAIL: "vidkri1504@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
