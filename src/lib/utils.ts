5f7772f8fd415170bb742c962b8849003c15d4e5
reader <143727212+devansh-pan@users.noreply.github.com>
2024-10-24 17:16:16 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,14 @@ export function readingTime(html: string) {
   const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
   return `${readingTimeMinutes} min read`;
 }
+// src/utils/slugify.js
+export function slugify(tag) {
+  return tag
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with dashes
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-') // Replace multiple dashes with single dash
+    .replace(/^-+/, '') // Trim dashes from the start
+    .replace(/-+$/, ''); // Trim dashes from the end
+    }
+  
