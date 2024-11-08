5427aa519371be28527efbf98c2dec28f15fc0d6
devansh-pan <vidkri1504@gmail.com>
2024-11-08 10:55:26 +0000

    vercl add

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://telugulokam.vercel.app",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
   },
+
+  output: "hybrid",
+  adapter: vercel(),
 });
